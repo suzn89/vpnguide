cf1a231938c9e7c879f5520fa46655b3992e59d0
suzn89 <ssostory@gmail.com>
2020-02-14 15:22:35 +0900

    접속기기 테스트

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,11 @@
 $(function() {
+  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+
+
+  if(!isMobile) {
+    $('.device').attr('href','window.html');
+  }
+
   $('.menubtn').click(function() {
     $(this).toggleClass('close');
     $('.nav').toggleClass('open');
