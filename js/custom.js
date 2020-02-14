41ad84a8b7bd0841ab13edf973b78812acbeefb2
suzn89 <ssostory@gmail.com>
2020-02-14 16:08:55 +0900

    해상도로 판별하게 수정

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,5 @@
 $(function() {
-  var filter = "win16|win32|win64|mac|macintel";
-  if ( navigator.platform ) { 
-    if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) {
-      $('.device').attr('href','tablet.html');
-      alert('mobile 접속');
-    } 
-  }
-
+  
   $('.menubtn').click(function() {
     $(this).toggleClass('close');
     $('.nav').toggleClass('open');
